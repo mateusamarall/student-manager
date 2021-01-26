928e77bdd820bddc070ac63268dc4134c0236ff9
mateusamarall <mateus.amaral018@gmail.com>
2021-01-26 08:38:49 -0300

    variável isLoggedIn agora está dinámica

--- src/routes/MyRoute.js
+++ src/routes/MyRoute.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 // essão função bloquea o acesso de usuário não autenticado a rotas privadas e redereciona para /login
 function MyRoute({ component: Component, isCLosed, ...rest }) {
-  const isLoggedIn = false;
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
   if (isCLosed && !isLoggedIn) {
     return (
