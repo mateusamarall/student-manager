453e240cfacc47f1a8e501069f0793811812c813
mateusamarall <mateus.amaral018@gmail.com>
2021-02-10 18:08:24 -0300

    creating the page for register a student

--- src/pages/Aluno/index.js
+++ src/pages/Aluno/index.js
@@ -1,12 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
 import { Container } from '../../styles/GlobalStyle';
+import { Form } from './styled';
 
-function Aluno() {
+function Aluno({ match }) {
+  const id = get(match, 'params.id', 0);
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <Container>
-      <h1>Aluno</h1>
+      <h1>{id ? 'Editar Aluno' : 'Novo Aluno'}</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          placeholder="Nome"
+        />
+
+        <input
+          type="text"
+          value={sobrenome}
+          onChange={(e) => setSobrenome(e.target.value)}
+          placeholder="Sobrenome"
+        />
+
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+
+        <input
+          type="number"
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+          placeholder="Idade"
+        />
+
+        <input
+          type="text"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+          placeholder="Peso"
+        />
+
+        <input
+          type="text"
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
+          placeholder="Altura"
+        />
+        <button type="submit">Enviar</button>
+      </Form>
     </Container>
   );
 }
+
+Aluno.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
 
 export default Aluno;
--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -9,7 +9,7 @@ import {
 } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import { Container } from '../../styles/GlobalStyle';
-import { AlunoContainer, ProfilePicture } from './styled';
+import { AlunoContainer, ProfilePicture, NovoAluno } from './styled';
 import api from '../../services/axios';
 import Loading from '../../components/Loading';
 
@@ -56,7 +56,7 @@ function Alunos() {
     <Container>
       <Loading isLoading={isLoading} />
       <h1>Alunos</h1>
-
+      <NovoAluno to="/aluno/">Novo Aluno</NovoAluno>
       <AlunoContainer>
         {alunos.map((aluno, index) => (
           <div key={aluno.id}>
